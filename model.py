2077dd1419648e6fd24ac64d84cc705198b30a1a
HTLiu <shomyliu@gmail.com>
2020-07-04 15:16:42 +0800

    add Bert-base-uncased into model

--- model.py
+++ model.py
@@ -3,54 +3,88 @@
 from allennlp.modules.elmo import Elmo, batch_to_ids
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
+from transformers import AutoModel, AutoTokenizer
 import numpy as np
 
+from encoder import Encoder
 
-class TextCNN(nn.Module):
+
+class Model(nn.Module):
     def __init__(self, opt):
 
-        super(TextCNN, self).__init__()
+        super(Model, self).__init__()
         self.opt = opt
         self.use_gpu = self.opt.use_gpu
 
-        if self.opt.emb_method == 'elmo':
+        if opt.emb_method == 'elmo':
             self.init_elmo()
         elif self.opt.emb_method == 'glove':
             self.init_glove()
-        elif self.opt.emb_method == 'elmo_glove':
-            self.init_elmo()
-            self.init_glove()
-            self.word_dim = self.opt.elmo_dim + self.opt.glove_dim
+        elif self.opt.emb_method == 'bert':
+            self.init_bert()
 
-        self.cnns = nn.ModuleList([nn.Conv2d(1, self.opt.num_filters, (i, self.word_dim)) for i in self.opt.k])
-        for cnn in self.cnns:
-            nn.init.xavier_normal_(cnn.weight)
-            nn.init.constant_(cnn.bias, 0.)
-        self.linear = nn.Linear(self.opt.num_filters * len(self.opt.k), self.opt.num_labels)
-        nn.init.xavier_uniform_(self.linear.weight)
-        nn.init.constant_(self.linear.bias, 0)
+        self.encoder = Encoder(opt.enc_method, self.word_dim, opt.hidden_size, opt.out_size)
+        self.cls = nn.Linear(opt.out_size, opt.num_labels)
+        nn.init.uniform_(self.cls.weight, -0.1, 0.1)
+        nn.init.uniform_(self.cls.bias, -0.1, 0.1)
         self.dropout = nn.Dropout(self.opt.dropout)
+
+    def forward(self, x):
+        if self.opt.emb_method == 'elmo':
+            word_embs = self.get_elmo(x)
+        elif self.opt.emb_method == 'glove':
+            word_embs = self.get_glove(x)
+        elif self.opt.emb_method == 'bert':
+            word_embs = self.get_bert(x)
+
+        x = self.encoder(word_embs)
+        x = self.dropout(x)
+        x = self.cls(x)    # batch_size * num_label
+        return x
+
+    def init_bert(self):
+        '''
+        initilize the Bert model
+        '''
+        self.bert_tokenizer = AutoTokenizer.from_pretrained(self.opt.bert_path)
+        self.bert = AutoModel.from_pretrained(self.opt.bert_path)
+        for param in self.bert.parameters():
+            param.requires_grad = False
+        self.word_dim = self.opt.bert_dim
 
     def init_elmo(self):
         '''
         initilize the ELMo model
         '''
         self.elmo = Elmo(self.opt.elmo_options_file, self.opt.elmo_weight_file, 1)
+        for param in self.elmo.parameters():
+            param.requires_grad = False
         self.word_dim = self.opt.elmo_dim
 
     def init_glove(self):
         '''
         load the GloVe model
         '''
-        self.word2id = np.load(self.opt.word2id_file).tolist()
+        self.word2id = np.load(self.opt.word2id_file, allow_pickle=True).tolist()
         self.glove = nn.Embedding(self.opt.vocab_size, self.opt.glove_dim)
-        emb = torch.from_numpy(np.load(self.opt.glove_file))
+        emb = torch.from_numpy(np.load(self.opt.glove_file, allow_pickle=True))
         if self.use_gpu:
             emb = emb.to(self.opt.device)
         self.glove.weight.data.copy_(emb)
         self.word_dim = self.opt.glove_dim
+
+    def get_bert(self, sentence_lists):
+        '''
+        get the ELMo word embedding vectors for a sentences
+        '''
+        sentence_lists = [' '.join(x) for x in sentence_lists]
+        ids = self.bert_tokenizer(sentence_lists, padding=True, return_tensors="pt")
+        inputs = ids['input_ids']
+        if self.opt.use_gpu:
+            inputs = inputs.to(self.opt.device)
+
+        embeddings = self.bert(inputs)
+        return embeddings[0]
 
     def get_elmo(self, sentence_lists):
         '''
@@ -76,20 +110,3 @@ class TextCNN(nn.Module):
 
         return embeddings
 
-    def forward(self, x):
-        if self.opt.emb_method == 'elmo':
-            word_embs = self.get_elmo(x)
-        elif self.opt.emb_method == 'glove':
-            word_embs = self.get_glove(x)
-        elif self.opt.emb_method == 'elmo_glove':
-            glove = self.get_glove(x)
-            elmo = self.get_elmo(x)
-            word_embs = torch.cat([elmo, glove], -1)
-
-        x = word_embs.unsqueeze(1)
-        x = [F.relu(cnn(x)).squeeze(3) for cnn in self.cnns]   # batch_size * num_filter * (max_length-3+1)
-        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
-        x = torch.cat(x, 1)
-        x = self.dropout(x)
-        x = self.linear(x)    # batch_size * num_label
-        return x
